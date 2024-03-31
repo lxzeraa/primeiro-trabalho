734758c37f7ba73a2750297c7a792c0fb7b55dee
lxzeraa <eusouluis20@gmail.com>
2024-03-30 21:28:05 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'primeiro-trabalho',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
